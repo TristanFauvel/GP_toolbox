ccc451825a5f95a85dadbe1e68b78f567be529f9
TristanFauvel <tristan.fauvel@hotmail.fr>
2022-03-07 18:54:06 +0100

    Renaming of the Gaussian function.

--- general_functions/Gaussian_fun.m
+++ general_functions/Gaussian_fun.m
@@ -1,10 +1,6 @@
-function [g, dgdx, dgdmu, dgdsigma] =  Gaussian(x, mu, sigma)
+function [g, dgdx, dgdmu, dgdsigma] =  Gaussian_fun(x, mu, sigma)
 
 g= normpdf(x,mu,sigma); %1/(sqrt(2*pi)*sigma)*exp(-(x-mu).^2/(2*sigma.^2))
-dgdx =-(x-mu)/(sigma.^2).*g; 
+dgdx =-(x-mu)/(sigma.^2).*g;
 dgdmu= -dgdx;
 dgdsigma = g.*(((x-mu)/sigma).^2-1)./sigma; %derivative of the gaussian
-
-
-
- 
